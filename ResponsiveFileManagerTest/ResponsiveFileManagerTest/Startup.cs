a5d5b6a678e38b7286f9b3f77f2a7f2e0c6bd57c
alex.toplijski <alex.toplijski@gmail.com>
2017-11-24 16:58:28 +0200

    Init repo

---
+++ ResponsiveFileManagerTest/ResponsiveFileManagerTest/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ResponsiveFileManagerTest.Startup))]
+namespace ResponsiveFileManagerTest
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
